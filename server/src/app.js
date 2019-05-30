f95c8084fe99cd465f91ba765092920a354e5430
Christopher Cahill <cthomascahill@gmail.com>
2019-05-29 22:50:34 -0700

    init

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+console.log('hello worlddd');
+
+app.get('/posts', (req, res) => {
+  res.send(
+    [{
+      title: "Hello World!",
+      description: "Hi there! How are you?",
+      oneOtherThing: "ok",
+      hi: "hi"
+
+    }]
+  )
+})
+
+app.listen(process.env.PORT || 8081)
